86ba73ce07299f5046aad949bd98c8fae65d79e7
Nikhil <nikhilsk98@gmail.com>
2023-11-05 00:08:20 +0530

    Second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images /alexa.png";
+import CortanaImage from "./images /cortana.png";
+import "bulma/css/bulma.css";
+import SiriImage from "./images /siri.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </section>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                desc="Alexa is created by Amazon"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@Cortana99"
+                image={CortanaImage}
+                desc="Cortana is created by Microsoft"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@Siri09"
+                image={SiriImage}
+                desc="Siri is created by Apple"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
